baf4e649f39f8ec3c53f0c5b4996dadf050a24f1
alagamai <alagamai.n@gmail.com>
2025-11-06 21:32:38 +0530

    update

--- autogen-python/test-folder/get-api-open-router.py
+++ autogen-python/test-folder/get-api-open-router.py
@@ -1,6 +1,11 @@
 import requests
+from dotenv import load_dotenv
 
-openrouter_api_key = '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'
+import os
+
+load_dotenv()
+
+openrouter_api_key = os.getenv("OPENROUTER_API_KEY")
 
 url = "https://openrouter.ai/api/v1/models/openai/gpt-oss-20b/endpoints"
 
